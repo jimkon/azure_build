a76d1292ccd268d06d36e3f49696c1454f89727e
jimkon <kontzedakis_93@hotmail.com>
2016-07-27 10:35:07 +0300

    hello world

--- app.js
+++ app.js
@@ -1,55 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const app  =  express();
-const router = require('./routes/api');
-const passport = require('passport');
-const flash = require("connect-flash");
-const session = require('express-session');
+const http = require('http');
 
-/*mongoose.connect('mongodb://localhost:27017/db_name', function (err) {
-    if (err) {
-        console.error(err, 'Error while connecting mongoose');
-    } else {
-        console.log('Successful mongoose connection');
-    }
-});*/
-mongoose.connect('mongodb://dimitris:dimitris@ds031948.mlab.com:31948/testdb_ctek', function (err) {
-    if (err) {
-        console.error(err, 'Error while connecting mongoose');
-    } else {
-        console.log('Successful mongoose connection');
-    }
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
 });
 
-
-//Load Middleware
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
-app.use('/api', router);
-
-app.use('/public', express.static('./client'));
-
-app.get('/auth', (req, res) => {
-    res.sendFile('index.html', {root: __dirname + '/client/auth'});
-});
-app.get('/*', (req, res) => {
-    res.sendFile('index.html', {root: __dirname + '/client/pre_auth'});
-});
-
-
-//Start the app
-mongoose.connection.once('open', () => {
-    app.listen(3000);
-    console.log('Listening at 3000');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
